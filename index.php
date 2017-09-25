6ad0a3d6cd19f5dec098df502b2ad5588a222d09
facudiaz7 <facu.diaz777@gmail.com>
2017-09-24 21:59:34 -0300

    Estructura basica. Header/Footer

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php require_once('php/cabecera.php')?>
+Aqui tengo el contenido de la pagina principal
+<?php require_once('php/footer_page.php')?>
---
+++ php/cabecera.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <title>Jcruz Ecommers</title>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <header>
+                <h1>Jcruz Ecommers</h1>
+                <h2>Subtitulo tienda</h2>
+            </header>
+            <section>
